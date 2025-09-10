93289db14f71a946a7d64664c111185b8a807302
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-10 21:49:20 +0000

    Code generation: 8b2000a0

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "HighFiver";
-export const PROJECT_DESCRIPTION = "by @adnum";
+export const PROJECT_DESCRIPTION = "Give high fives to fellow Farcaster users with a simple tap";
 export const PROJECT_CREATOR = "adnum";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
